3acb76801d0d1d33fc474c130fd1730a4d2b90d5
Irfan-ij <irfanjumail694@gmail.com>
2026-09-08 14:32:03 +0530

    Update play.py

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -16,7 +16,7 @@ data=urllib.request.urlopen(request,timeout=5).read().decode("utf-8",errors="ign
 
 ids=re.findall(r'"videoID":"([^"]+)")"' , data)
 
-return id[0] if ids else none 
+return id[*0] if ids else none 
 except Exception
 return none
 
@@ -36,4 +36,22 @@ query = command
 for pattern in patterns:
 
   match = re.search(pattern,text)
+
+if match:
+
+  query=match.group(1)
+  break
+
+  query=query.strip()
+
+video_id=get_vid(query)
+
+if not video_id:
+  return none
+
+return(
+  "https://www.youtube.com/embed/ "
+  +video_id
+  +"?autoplay=1&mute=0"
+)
 
