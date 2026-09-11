1f42468383e8f48fdfa8f88d82f4c7886952bda4
Irfan-ij <irfanjumail694@gmail.com>
2026-09-11 09:30:35 +0530

    Update __init__.py

--- app/gmail/__init__.py
+++ app/gmail/__init__.py
@@ -1,2 +1,2 @@
-from .gmail_write import is_gmail_command, extract_gmail, create_gmail_url
+from .gmail_write import is_email_command, extract_gmail, create_gmail_url
 from .gmail_gen import generation_email_with_gemini
